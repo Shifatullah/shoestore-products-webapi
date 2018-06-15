18513ba8632cec036dad381b2563c9e3caf74033
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2018-06-16 04:43:09 +1000

    no message

---
+++ ShoeStore.ProductsApi.Domain/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoeStore.ProductsApi.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public int Rank { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string LongDescription { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public int Stock { get; set; }        
+    }
+}
---
+++ ShoeStore.ProductsApi/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using ShoeStore.ProductsApi.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShoeStore.ProductsApi.Controllers
+{
+    [RoutePrefix("api/v1")]
+    public class ProductsController : ApiController
+    {
+
+        [Route("products")]
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product()
+            {
+                Id = 1,
+                Name = "iPhone 8",
+                Code = "iP8",
+                Cost = 1000,
+                ShortDescription = "iPhone 8 mobile phone",
+                LongDescription = "iPhone 8 mobile phone is better",
+                Rank = 1,
+                Stock = 3
+            });
+
+            products.Add(new Product()
+            {
+                Id = 2,
+                Name = "iPhone X",
+                Code = "iPX",
+                Cost = 2000,
+                ShortDescription = "iPhone X mobile phone",
+                LongDescription = "iPhone X mobile phone is better",
+                Rank = 2,
+                Stock = 5
+            });
+
+            return products;
+        }
+    }
+}
